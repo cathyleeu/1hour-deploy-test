f9d27c5003e48a111c44d27911029e4f8c8644fc
kmjnnhyk <rlawlsgur620@gmail.com>
2022-06-19 03:37:56 +0900

    feat: sidebar layout 구현

---
+++ components/Sidebar/Sidebar.tsx
@@ -0,0 +1,15 @@
+import styles from './Sidebar.module.scss'
+
+const Sidebar: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo} />
+      <div className={styles.select_group}>
+        <select className={styles.learning} />
+        <select className={styles.review} />
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
+import '../styles/reset.scss'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Sidebar from '../components/Sidebar'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Sidebar />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
