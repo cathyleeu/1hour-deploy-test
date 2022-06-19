69846d931d139397d68a12dcf6627635faae7e09
yena <center7332@naver.com>
2022-06-19 21:14:50 +0900

    feat: question-card component added

--- components/common/tag.tsx
+++ components/common/tag.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const Tag = ({ content }: Props) => {
   return (
-    <div className="text-blue px-[9px] py-[6px] w-fit border border-blue rounded-[12px] font-poppins text-[10px]">{`#${content}`}</div>
+    <div className="text-blue px-[9px] py-[6px] w-fit border border-blue rounded-[12px] font-poppins text-[10px] whitespace-nowrap">{`#${content}`}</div>
   );
 };
 
