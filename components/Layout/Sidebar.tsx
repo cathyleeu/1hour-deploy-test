c7edd9072d02ef71d79d6e288fa574a21929a511
kmjnnhyk <rlawlsgur620@gmail.com>
2022-06-20 05:53:00 +0900

    feat: sidebar dropdown 작업 완료

--- components/Layout/Sidebar.tsx
+++ components/Layout/Sidebar.tsx
@@ -1,3 +1,4 @@
+import SideDropdown from '@components/common/side-dropdown';
 import Link from 'next/link';
 
 {
@@ -10,44 +11,31 @@ interface Category {
 }
 export default function Sidebar({}) {
   const categories: Category[] = [
-    { name: '프론트엔드', params: 'frontend' },
-    { name: '서버/백엔드', params: 'backend' },
-    { name: 'Mobile', params: 'mobile' },
-    { name: 'Data Science', params: 'datascience' },
-    { name: '데브옵스', params: 'devops' },
+    { name: '프론트엔드', params: '/categories/frontend' },
+    { name: '서버/백엔드', params: '/categories/backend' },
+    { name: 'Mobile', params: '/categories/mobile' },
+    { name: 'Data Science', params: '/categories/datascience' },
+    { name: '데브옵스', params: '/categories/devops' },
+  ];
+  const reviewCategories: Category[] = [
+    { name: '북마크', params: '/bookmark' },
+    { name: '연습문제 풀기', params: '/dev-quiz' },
   ];
 
   return (
     <div className="flex flex-col items-center h-full bg-gray-dark text-white">
-      {/* logo */}
-      <div className="w-full ml-14 mt-20">
+      {/* 로고 */}
+      <div className="w-full ml-14 mt-24 mb-20">
         <Link href="/">
           <button className="w-[108px] h-[68px] bg-[url('/assets/images/sidebar/logo.svg')]" />
         </Link>
       </div>
 
-      {/* TODO : 드롭다운 */}
-      <div className="text-2xl mt-16 w-60 h-12 bg-sky-600">학습하기</div>
-      {/* TODO : 카테고리 */}
-      <ul>
-        {categories.map((category: Category, i: number) => (
-          <li key={i}>
-            <Link href={`/categories/${category.params}`}>{category.name}</Link>
-          </li>
-        ))}
-      </ul>
+      {/* 학습하기 */}
+      <SideDropdown list={categories} src={'/assets/images/sidebar/book.svg'} title={'학습하기'} />
 
-      {/* TODO : 드롭다운 */}
-      <div className="text-2xl mt-16 w-60 h-12 bg-sky-600">복습하기</div>
-      <Link href="/bookmark">
-        <p>북마크</p>
-      </Link>
-      <Link href="/dev-quiz">
-        <p>연습문제풀기</p>
-      </Link>
-      <Link href="/post-quiz">
-        <p>글쓰기</p>
-      </Link>
+      {/* 복습하기 */}
+      <SideDropdown list={reviewCategories} src={'/assets/images/sidebar/review.svg'} title={'복습하기'} />
     </div>
   );
 }
---
+++ components/common/side-dropdown.tsx
@@ -0,0 +1,74 @@
+import ArrowIcon from './arrow-icon';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRef, useState } from 'react';
+import { useEffect } from 'react';
+
+interface category {
+  name: string;
+  params: string;
+}
+interface Props {
+  src: string;
+  title: string;
+  list: category[];
+}
+
+const SideDropdown = ({ src, title, list }: Props) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [selectedList, setSelectedList] = useState<string>('');
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const handleClick = (e: any) => {
+    const target = e.target as Element;
+    setSelectedList(target.innerHTML);
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', clickOutside);
+    return () => {
+      document.removeEventListener('mousedown', clickOutside);
+    };
+  });
+
+  const clickOutside = (e: any) => {
+    if (isOpen && !dropdownRef.current?.contains(e.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  return (
+    <div ref={dropdownRef} className={'dropdown'}>
+      <div className={'flex flex-col items-center'}>
+        {/* button */}
+        <button
+          className={`flex items-center rounded-xl  mb-4 px-4 py-3.5 w-60 h-12 bg-${
+            isOpen ? 'blue' : 'gray'
+          } text-base font-bold `}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <Image src={src} alt="학습하기" width={23} height={22} />
+          <p className={'mx-3 w-36 text-left'}>{title}</p>
+          <ArrowIcon color={'white'} width={13} height={6} />
+        </button>
+
+        {/* list */}
+        {isOpen && (
+          <ul className={'w-48 h-full'}>
+            {list.map((data, id) => (
+              <Link href={data.params} key={id}>
+                <li
+                  onClick={handleClick}
+                  className={`text-${selectedList === data.name ? 'blue' : 'white'} h-11 cursor-pointer`}
+                >
+                  {data.name}
+                </li>
+              </Link>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SideDropdown;
