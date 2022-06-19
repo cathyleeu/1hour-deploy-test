64e3bcf55b76b353256ca42c456329bcd698cb68
Cathy Lee <leeyu823@gmail.com>
2022-06-19 18:05:37 +0900

    feat: add input textarea base component

--- pages/dev-quiz.tsx
+++ pages/dev-quiz.tsx
@@ -1,13 +1,27 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import {Input, TextArea} from '@components/Form'
 
 const DevQuiz: NextPage = () => {
   return (
-    <div>
+    <div className="w-full">
       <Head>
         <title>1Hour - Dev Quiz</title>
       </Head>
       연습문제페이지
+      <div>
+        <Input 
+          aria-label="Demo input"
+          placeholder="type something"
+          className="rounded-large w-3/5"
+        />
+        <TextArea
+          aria-label="Demo input"
+          placeholder="type something"
+          className="rounded-large w-3/5"
+        />
+      </div>
+      
       {/* TODO: DevQuiz */}
     </div>
   )
