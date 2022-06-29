1c2dcfdc694333de048a8f4005bc11fefe36d4a0
Cathy Lee <leeyu823@gmail.com>
2022-06-29 19:57:55 +0900

    remove : container tag

--- pages/dev-quiz.tsx
+++ pages/dev-quiz.tsx
@@ -1,21 +1,20 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
-import { Container } from '@components/Layout';
+import { QuizPhase } from '@components/Quiz'
 import QuizProvier from '@components/Quiz/QuizContext';
-import QuizPhase from '@components/Quiz/QuizPhase';
 
 
 const DevQuiz: NextPage = () => {
   return (
-    <QuizProvier>
-      <Container>
-        <Head>
-          <title>1Hour - Dev Quiz</title>
-        </Head>
+    <>
+      <Head>
+        <title>1Hour - Dev Quiz</title>
+      </Head>
+      <QuizProvier>
         <QuizPhase />
-      </Container>
-    </QuizProvier>
+      </QuizProvier>
+    </>
   );
 };
 
