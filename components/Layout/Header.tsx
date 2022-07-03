3fd9dc409677675f5574dbd92a844a823aa42cc4
yena <center7332@naver.com>
2022-07-03 16:02:32 +0900

    feat: add login modal

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -3,34 +3,47 @@ import { Hamburger, Logo } from '@components/common/Icon';
 import { useResponsiveContext } from '@components/Layout/ResponsiveContext';
 import Avatar from './avatar';
 import { useLogin } from 'lib/hooks';
+import dynamic from 'next/dynamic';
+
+const LoginModal = dynamic(() => import('./login-modal'));
 
 export default function Header({ children }: BaseProps) {
   const { IsMobileScreen, setIsMenuOpen } = useResponsiveContext();
-  const { isAuthorize, login } = useLogin();
+  const { isAuthorize, isOpenLogin, toggleOpenLogin } = useLogin();
 
   const openSidebar = () => {
     setIsMenuOpen(true);
   };
+
   return (
-    <div className="h-16 lg:h-24 lg:py-4 mb-6 lg:my-4 w-full flex items-center">
-      <div
-        className={`
-          mr-auto flex items-center
-          ${IsMobileScreen ? 'opacity-100' : 'opacity-0'}
+    <>
+      <div className="h-16 lg:h-24 lg:py-4 mb-6 lg:my-4 w-full flex items-center">
+        <div
+          className={`
+        mr-auto flex items-center
+        ${IsMobileScreen ? 'opacity-100' : 'opacity-0'}
         `}
-      >
-        <Hamburger className="h-4 mr-4" handleClick={openSidebar} />
-        <Logo className="h-8" />
+        >
+          <Hamburger className="h-4 mr-4" handleClick={openSidebar} />
+          <Logo className="h-8" />
+        </div>
+        <div className="ml-auto">
+          {isAuthorize ? (
+            <Avatar />
+          ) : (
+            <p
+              onClick={(e) => {
+                e.stopPropagation();
+                toggleOpenLogin();
+              }}
+              className="py-4 font-bold text-xl cursor-pointer"
+            >
+              로그인
+            </p>
+          )}
+        </div>
       </div>
-      <div className="ml-auto">
-        {isAuthorize ? (
-          <Avatar />
-        ) : (
-          <p onClick={login} className="py-4 font-bold text-xl cursor-pointer">
-            로그인
-          </p>
-        )}
-      </div>
-    </div>
+      <LoginModal {...{ isOpenLogin, toggleOpenLogin }} />
+    </>
   );
 }
