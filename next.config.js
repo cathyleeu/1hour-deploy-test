1d7b0c257d41dee070bfccc4d54c03393a457a54
cathyleeu <leeyu823@gmail.com>
2022-07-08 17:25:19 +0900

    setup: add github image domain

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: [
+      'avatars.githubusercontent.com'
+    ]
+  },
   reactStrictMode: true,
   images: {
     domains: ['avatars.githubusercontent.com'],
