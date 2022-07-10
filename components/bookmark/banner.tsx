58f8362ae797808f215cd4b39768d70ba0699839
cathyleeu <leeyu823@gmail.com>
2022-07-11 04:45:26 +0900

    fix(banner): mobile layout

--- components/bookmark/banner.tsx
+++ components/bookmark/banner.tsx
@@ -10,16 +10,17 @@ const BookmarkBanner = ({ name }: Props) => {
     <div
       className="
         bg-orange
-        w-full flex flex-wrap justify-center items-center
+        w-full flex flex-wrap justify-center md:justify-between items-center
         rounded-large px-6 lg:px-10 py-6 
       "
     >
       <section className="w-full md:w-1/2">
-        <h1 className="font-bold text-[55px] leading-[60px]">
-          <span className="text-black">{name}</span>
-          의
-          <br />
-          북마크
+        <h1 className="font-bold text-3xl md:text-6xl">
+          <span>{name}</span>
+          <span className='text-white'>
+            <br className='block md:hidden'/>의 
+            <br className='hidden md:block'/> 북마크
+          </span>
         </h1>
         <p className="mt-[16px]">
           {name}님이 저장해두신 북마크를 모두 모아봤어요.
@@ -27,8 +28,16 @@ const BookmarkBanner = ({ name }: Props) => {
           복습을 시작해보세요.
         </p>
       </section>
-      <section className="relative w-[244px] h-[248px]">
-        <Image src="/assets/images/banner/bookmark.png" layout="fill" objectFit="contain" priority alt="banner" />
+      <section className="relative w-[183px] md:w-[244px]">
+        <Image 
+          src="/assets/images/banner/bookmark.png" 
+          layout="responsive" 
+          objectFit="contain" 
+          priority 
+          width={244}
+          height={248}
+          alt="banner" 
+        />
       </section>
     </div>
   );
