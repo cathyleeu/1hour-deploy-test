ad4f8233f5bf4cd215397e60a6e1488e638848ec
cathyleeu <leeyu823@gmail.com>
2022-07-17 12:12:25 +0900

    feat : add get qna api

--- api/oneHourAPI/index.ts
+++ api/oneHourAPI/index.ts
@@ -1,27 +1,8 @@
-import { useEffect, useState } from 'react';
 import customAxios from '../axios/customAxios';
 import openHourUrl from '../axios/api.config';
 import { CategoryId, QnAType } from '../types';
 
-// export const GetCategories = () => {
-//   // Promise{<pending>} 형태의 반환값이 리턴되기 때문에
-//   // 체인 안에서 useState에 데이터를 저장한 후 리턴
-//   const [data, setData] = useState();
 
-//   useEffect(() => {
-//     customAxios
-//       .get(openHourUrl.GET_CATEGORY)
-//       .then((res) => {
-//         return res.data;
-//       })
-//       .then((data) => {
-//         setData(data);
-//       })
-//       .catch((e) => console.log('Get Categories ERROR!', e));
-//   }, []);
-
-//   return data;
-// };
 export const GetCategories = async () => {
   const { data } = await customAxios.get(openHourUrl.GET_CATEGORY);
   return data;
@@ -32,6 +13,15 @@ export const GetTags = async (category_id: CategoryId) => {
   return data;
 };
 
+export const GetQnA =async (category_id:CategoryId, user_id:string) => {
+  const { data } = await customAxios.get(openHourUrl.GET_QNA, {
+    params: {
+      user_id,
+      category_id
+    }
+  });
+  return data;
+}
 export const PostQnA = async (data: QnAType, user_id: string) => {
   try {
     const response = await customAxios.post(openHourUrl.POST_QNA, data, { params: { user_id: user_id } });
