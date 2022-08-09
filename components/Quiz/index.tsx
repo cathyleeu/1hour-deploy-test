54dce5cf51be2017cfb474a6189408ebb9e79e14
cathyleeu <leeyu823@gmail.com>
2022-08-09 22:08:17 +0900

    fix: set quiz num

--- components/Quiz/index.tsx
+++ components/Quiz/index.tsx
@@ -3,7 +3,7 @@ export { default as AnswerCard } from './AnswerCard';
 export { default as ResultCard } from './ResultCard';
 
 export { default as IconText } from './IconText';
-export { default as TimerInput } from './TimerInput';
+export { default as NumberInput } from './NumberInput';
 export { default as QuizTimer } from './QuizTimer';
 
 export { default as QuizPhase} from './QuizPhase';
