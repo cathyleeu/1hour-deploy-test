8d9aca6b662f93f856749b91aa020b47f6389f36
cathyleeu <leeyu823@gmail.com>
2022-08-30 04:36:30 +0900

    fix: add api route

---
+++ lib/api.ts
@@ -0,0 +1,23 @@
+export async function responseAPI(endpoint:string, options = {}) {
+  let response, error
+  try {
+    const data = await fetch(endpoint, options)
+    response = await data.json();
+  } catch (err) {
+    error = err;
+  }
+  return [response, error]
+}
+
+export const oneHourUrl = {
+  GET_CATEGORY: 'http://localhost:3000/api/categories',
+  GET_TAGS: 'http://localhost:3000/api/tags',
+  GET_TAGS_BY_CATEGORY: (category:string) => `http://localhost:3000/api/tags/${category}`,
+  GET_TAG_BY_ID: (id:string) => `http://localhost:3000/api/tags/${category}/${id}`,
+  POST_QNA: 'http://localhost:3000/api/qna',
+  GET_QNA: 'http://localhost:3000/api/qna',
+  GET_QNA_BY_CATEGORY: (category:string) => `http://localhost:3000/api/qna/${category}`,
+  GET_USER_BOOKMARKS: (uid:string) => `http://localhost:3000/api/favor/${uid}/bookmarks`,
+  GET_USER_LIKES: (uid:string) => `http://localhost:3000/api/favor/${uid}/likes`,
+  GET_USER_FAVORITES: (uid:string) => `http://localhost:3000/api/favor/${uid}`,
+};
