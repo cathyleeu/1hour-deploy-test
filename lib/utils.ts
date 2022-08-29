c5bf240ee439edb270dee9ceec76161aac8c68d3
cathyleeu <leeyu823@gmail.com>
2022-08-30 04:38:25 +0900

    feat: add common response api

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,36 +1,3 @@
-export const categoryBanner: CategoryBanner = {
-  frontend: {
-    title: 'Front-End',
-    content:
-      '프론트엔드 개발자는 백엔드 API에서 가져온 데이터의 출력, 입력을 통한 비즈니스 로직 구성과 사용자와 대화하는 사용자 인터페이스 부분을 작업하는 개발자를 말해요.',
-    src: '/assets/images/banner/frontend.png',
-  },
-  backend: {
-    title: 'Back-End',
-    content:
-      '백엔드는 웹 애플리케이션의 사용자가 보지 못하는 영역인 서버나 데이터베이스를 관리해요. 데이터를 관리하거나 서버를 운영하는 일을 한답니다.',
-    src: '/assets/images/banner/backend.png',
-  },
-  devops: {
-    title: 'Dev Ops',
-    content:
-      '데브옵스는 소프트웨어의 개발과 운영의 합성어로서, 소프트웨어 개발자와 정보기술 전문가 간의 소통, 협업 및 통합을 강조하는 개발 환경이나 문화를 말해요.',
-    src: '/assets/images/banner/devops.png',
-  },
-  mobile: {
-    title: 'Mobile',
-    content:
-      '모바일 개발자는 스마트폰에서 가동되는 앱 프로그램을 개발하고 개발된 프로그램에 대한 오류를 수정하고 새로운 버전의 프로그램을 개발하는 일을 해요.',
-    src: '/assets/images/banner/mobile.png',
-  },
-  datascience: {
-    title: 'Data Science',
-    content:
-      '데이터 사이언스는 분석 방법, 도메인 전문성 및 기술의 융합을 통해 데이터에서 패턴을 찾고, 추출하고, 표면화하는 것을 말해요.',
-    src: '/assets/images/banner/datascience.png',
-  },
-};
-
 export const tagByCategory: TagValue = {
   frontend: [
     'javascript',
@@ -112,6 +79,7 @@ export const tagByCategory: TagValue = {
     'r',
     'tensoflow',
   ],
+  // computerscience: []
 };
 
 export const RandomQuizList = [
@@ -193,4 +161,15 @@ export function generateUUID() { // Public Domain/MIT
       }
       return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
   });
+}
+
+export async function responseAPI(endpoint:string) {
+  let response, error
+  try {
+    const data = await fetch(endpoint)
+    response = await data.json();
+  } catch (err) {
+    error = err;
+  }
+  return [response, error]
 }
